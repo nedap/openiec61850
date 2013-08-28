df7c70e18dc270fb2097bfa3b64ce4b683713eba
Pieter Bos <pieter.bos@nedap.com>
2013-08-28 13:52:37 +0200

    Implement Quality model

---
+++ src/common/src/main/java/org/openiec61850/bitstringhelper/Validity.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openiec61850.bitstringhelper;
+
+/**
+ *
+ * @author pieter.bos
+ */
+public enum Validity { GOOD, INVALID, RESERVED, QUESTIONABLE;
+}
